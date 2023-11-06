8e91fb1a9d7725908184df376ce8551a17ee7456
MakEva <sher.evikm@gmail.com>
2023-11-06 12:45:14 +0200

    final

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,20 @@
 
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector("#vimeo-player");
+const player = new Player(iframe);
+
+const currentTime = function ({ seconds }) {
+    localStorage.setItem("videoplayer-current-time", seconds);
+};
+player.on('timeupdate', throttle(currentTime, 1000));
+
+const newCurrentTime = localStorage.getItem("videoplayer-current-time");
+if (newCurrentTime) {
+    player.setCurrentTime(newCurrentTime);
+}
+
+
+
+    
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from "lodash.throttle";
 
+const ref = {
+    form: document.querySelector(".feedback-form"),
+    input: document.querySelector(".feedback-form input"),
+    message: document.querySelector(".feedback-form textarea"),
+};
+const STORAGE_KEY = "feedback-form-state";
+let formDataObj = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+ref.form.addEventListener("submit", onFormSubmit);
+ref.form.addEventListener("input", throttle(onFormInput, 500));
+
+formChecking();
+
+function formChecking() {
+    let savedStorage = localStorage.getItem(STORAGE_KEY);
+    if (savedStorage) {
+        let parsedStorage = JSON.parse(savedStorage);
+        ref.input.value = parsedStorage.email || "";
+        ref.message.value = parsedStorage.message || "";
+    }
+};
+
+function onFormInput(evt) {
+    formDataObj[evt.target.name] = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formDataObj));
+}
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    if (ref.input.value === "" || ref.message.value === "") {
+        alert("Заповніть, будь ласка, всі поля форми!");
+        return;
+    }
+    ref.form.reset();
+    console.log(formDataObj);
+    localStorage.removeItem(STORAGE_KEY);
+    formDataObj = {};
+    
+}
+
+
+
+
+// JSON.parse(localStorage.getItem(STORAGE_KEY)) ||
